36371e88c53e6e45646859dd529ffb288a7c9714
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-12 19:18:06 -0300

    iniciando criação de testes

---
+++ sistemaPagamentos/src/entity/Funcionario.java
@@ -0,0 +1,19 @@
+package entity;
+
+public class Funcionario {
+
+    private String nome;
+    private int horasTrabalhadas;
+    private Double valorHora;
+
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, int horasTrabalhadas, Double valorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+
+}
--- sistemaPagamentos/src/test/FuncionarioTest.java
+++ sistemaPagamentos/src/test/FuncionarioTest.java
@@ -1,4 +1,29 @@
 package test;
 
+import entity.Funcionario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FuncionarioTest {
+
+    //arrange
+    //act
+    //assign
+
+    private Funcionario funcionario;
+    @BeforeEach
+    public void inicializaCenario(){
+        funcionario = new Funcionario("Julia",160,250.50);
+    }
+
+    @Test
+    public void testaSetterNome(){
+        funcionario.setNome("Julya");
+        Assertions.assertEquals("Julya", funcionario.getNome());
+    }
+
+
+
 }
