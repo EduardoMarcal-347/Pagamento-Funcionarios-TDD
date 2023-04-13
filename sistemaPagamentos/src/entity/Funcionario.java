75dee3e2e14799d6b18d5e760fb2dca451dded4f
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-13 19:09:22 -0300

    criando testes para contrutores e setters com valores que não satisfazem

--- sistemaPagamentos/src/entity/Funcionario.java
+++ sistemaPagamentos/src/entity/Funcionario.java
@@ -16,5 +16,5 @@ public class Funcionario {
         this.valorHora = valorHora;
     }
 
-
+    
 }
--- sistemaPagamentos/src/test/FuncionarioTest.java
+++ sistemaPagamentos/src/test/FuncionarioTest.java
@@ -20,10 +20,31 @@ public class FuncionarioTest {
 
     @Test
     public void testaConstrutor(){
-        Funcionario funcionario = new Funcionario("Fernanda",200,125.50);
-        Assertions.assertEquals("Fernanda", funcionario.getNome());
-        Assertions.assertEquals(200, funcionario.getHorasTrabalhadas());
-        Assertions.assertEquals(125.50, funcionario.getValorHora());
+        String nome = "Fernanda";
+        int horasTrabalhadas = 40;
+        Double valorHoras = 52.8;
+
+        Funcionario funcionarioA = new Funcionario(nome, horasTrabalhadas, valorHoras);
+
+        Assertions.assertEquals("Fernanda", funcionarioA.getNome());
+        Assertions.assertEquals(40, funcionarioA.getHorasTrabalhadas());
+        Assertions.assertEquals(52.8, funcionarioA.getValorHora());
+    }
+
+    @Test
+    public void testaConstrutorDadosNaoSatisfazem(){
+        String nome = "funcionario";
+
+        //valor maximo de horas é 40
+        int horasTrabalhadas = 41;
+
+        //valor valido por hora trabalhada está entre 4% e 10% de um salário minimo (R$1320)
+        Double valorHoras = 39.6;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Funcionario funcionarioA = new Funcionario(nome, horasTrabalhadas, valorHoras);
+            });
+
     }
 
     @Test
@@ -33,9 +54,22 @@ public class FuncionarioTest {
     }
 
     @Test
+    public void testaSetterNomeParametroVazio(){
+        Assertions.assertThrows(Exception.class, () -> {
+            funcionario.setNome("");
+        });
+    }
+
+    @Test
     public void testaSetterHorasTrabalhadas(){
         funcionario.setHorasTrabalhadas(140);
         Assertions.assertEquals(140, funcionario.getHorasTrabalhadas());
+    }
+    @Test
+    public void testaSetterHorasTrabalhadasParametroNegativo(){
+        Assertions.assertThrows(Exception.class, () -> {
+            funcionario.setHorasTrabalhadas(-1);
+        });
     }
 
     @Test
@@ -43,6 +77,13 @@ public class FuncionarioTest {
         funcionario.setValorHora(200.00);
         Assertions.assertEquals(200.00,funcionario.getValorHora());
     }
-    
+
+    @Test
+    public void testaSetterValorHoraParametroNegativo(){
+        Assertions.assertThrows(Exception.class, () -> {
+            funcionario.setValorHora(-1.0);
+        });
+    }
+
 
 }
